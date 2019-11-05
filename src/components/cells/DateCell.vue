7f9e28252bffc8b3ca8f46f4a4e7b379c4773ffa
Patrick Shaw <sixtey7@gmail.com>
2019-11-05 07:56:51 -0500

    add more cells to be used by the tables

---
+++ src/components/cells/DateCell.vue
@@ -0,0 +1,22 @@
+<template>
+    <td class="text-xs-center">{{ dateToShow | formatDate }}</td>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+    name: "DateCell",
+    props: {
+        dateToShow: Date
+    },
+    filters: {
+        formatDate: function(value) {
+            if (!value) {
+                return;
+            }
+            return moment.utc(value).format('MMM DD, YYYY');
+        }
+    }
+}
+</script>
---
+++ src/components/cells/NumberCell.vue
@@ -0,0 +1,12 @@
+<template>
+    <td class="text-xs-center">{{ numberToShow }}</td>
+</template>
+
+<script>
+export default {
+    name: "NumberCell",
+    props: {
+        numberToShow: Number
+    }
+}
+</script>

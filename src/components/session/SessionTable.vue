fa167fa54f36555a806d8060ee87d263b7a7ab12
Patrick Shaw <sixtey7@gmail.com>
2019-11-12 06:00:00 -0500

    add buttons to the session table

--- src/components/session/SessionTable.vue
+++ src/components/session/SessionTable.vue
@@ -20,13 +20,14 @@
                     <DateCell :dateToShow="props.item.date" />
                     <StringCell :stringToShow="props.item.game_id" />
                     <NumberCell :numberToShow="props.item.players.length" />
-                    <DateCell :dateToShow="props.item.date" />
+                    <ButtonCell class="button-cell" :id="props.item.id" :buttons="buttons" @click="buttonClicked"/>
                 </tr>
             </template>
         </v-data-table>
     </div>
 </template>
 <script>
+import ButtonCell from '../cells/ButtonCell.vue';
 import DateCell from '../cells/DateCell.vue';
 import NumberCell from '../cells/NumberCell.vue';
 import StringCell from '../cells/StringCell.vue';
@@ -36,6 +37,7 @@ import Vue from 'vue';
 export default {
        name: 'SessionTable',
        components: {
+           ButtonCell,
            DateCell,
            NumberCell,
            StringCell
@@ -43,6 +45,53 @@ export default {
        props: {
            sessions: Array,
            logger: Vue.logger
+       },
+       data() {
+           return {
+               buttons: [
+                   {
+                       name: 'edit',
+                       iconToShow: 'edit'
+                   },
+                   {
+                       name: 'delete',
+                       iconToShow: 'delete'
+                   },
+                   {
+                       name: 'add_session',
+                       iconToShow: 'add_circle_outline'
+                   }
+               ]
+           }
+       },
+       methods: {
+           buttonClicked(buttonName, itemIdClicked) {
+               this.logger.info(buttonName + ' was clicked for item: ' + itemIdClicked);
+               if (buttonName === 'edit') {
+                   this.handleEdit(itemIdClicked);
+               }
+               else if (buttonName === 'delete') {
+                   this.handleDelete(itemIdClicked);
+               }
+               else if (buttonName === 'add_session') {
+                   this.handleAddSession(itemIdClicked);
+               }
+               else {
+                this.logger.error('Got an invalid action from button callback: ' + buttonName);
+               }
+           },
+           handleEdit(itemIdClicked) {
+               this.logger.info('doing an edit for item: ' + itemIdClicked);
+               this.$emit('edit', itemIdClicked);
+           },
+           handleDelete(itemIdClicked) {
+               this.logger.info('doing a delete for item: ' + itemIdClicked);
+               this.$emit('delete', itemIdClicked);
+           },
+           handleAddSession(itemIdClicked) {
+               this.logger.info('adding a session for item ' + itemIdClicked);
+               this.$emit('add_session', itemIdClicked);
+           }
        }
 }
 </script>
